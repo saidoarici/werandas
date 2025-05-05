d6933cbf631b09209106efd4f9e7cfbaabc975b2
“Saido” <saidnahal.197@gmail.com>
2025-05-05 19:58:30 +0300

    değişiklik

--- app.py
+++ app.py
@@ -191,6 +191,9 @@ def inject_now():
     from datetime import datetime
     return {'datetime': datetime}
 
+
+
+
 # Bu kısım sadece doğrudan python ile çalıştırıldığında çalışır
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4444)
